28e2633247aaee63fc7d9a2aa1b4b8f72d646a66
salvah22 <salvah22@gmail.com>
2022-04-11 15:13:43 +0200

    colors_update

--- colors.py
+++ colors.py
@@ -1,67 +1,49 @@
-##Raster=group
-##Generate unique values style=name
-##Raster_to_extract_unique_values=raster
-##round_values_to_ndigits=number 0
+# tested with QGIS 3.24.1 & Python 3.10.2
 
-from osgeo import gdal
-from random import randint
-import math
-from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from qgis.core import *
-from qgis.utils import iface
 
-
-sorted_values = list(range(1,31))
 # Now load the layer and apply styling
 prj = QgsProject.instance()
-layer = prj.mapLayersByName('Clipped (mask)')
-#layer = QgsProcessingUtils.mapLayerFromString(
-print(layer)
+layer = prj.mapLayersByName('Amon_CNRM_CM6_1_HR_ssp585_r1i1p1f2_gr_201501_21001_koppen') # por algun motivo no le gustan nombres con guiones
 qCRS = QgsColorRampShader()
 
 # Build the colour ramp using random colours
-colList = [QColor(150, 0, 0, 255),
-           QColor(255, 0, 0, 255),
-           QColor(255, 204, 204, 255),
-           QColor(255, 204, 0, 255),
-           QColor(255, 255, 100, 255),
-           QColor(204, 141, 20, 255),
-           QColor(204, 170, 84, 255),
-           QColor(0, 255, 0, 255),
-           QColor(150, 255, 0, 255),
-           QColor(200, 255, 0, 255),
-           QColor(180, 100, 0, 255),
-           QColor(150, 100, 0, 255),
-           QColor(90, 60, 0, 255),
-           QColor(0, 50, 0, 255),
-           QColor(0, 80, 0, 255),
-           QColor(0, 120, 0, 255),
-           QColor(255, 110, 255, 255),
-           QColor(255, 180, 255, 255),
-           QColor(230, 200, 255, 255),
-           QColor(200, 200, 200, 255),
-           QColor(200, 180, 255, 255),
-           QColor(154, 127, 179, 255),
-           QColor(135, 89, 179, 255),
-           QColor(111, 36, 179, 255),
-           QColor(50, 0, 50, 255),
-           QColor(100, 0, 100, 255),
-           QColor(200, 0, 200, 255),
-           QColor(200, 20, 133, 255),
-           QColor(100, 255, 255, 255),
-           QColor(100, 150, 255, 255),
-           QColor(255, 153, 153, 255),
-           QColor(0, 0, 0, 255)]
+diction = {"Af": QColor(150, 0, 0, 255),
+           "Am": QColor(255, 0, 0, 255),
+           "Aw": QColor(255, 204, 204, 255),
+           "BWh":QColor(255, 204, 0, 255),
+           "BWk":QColor(255, 255, 100, 255),
+           "BSh":QColor(204, 141, 20, 255),
+           "BSk":QColor(204, 170, 84, 255),
+           "Csa":QColor(0, 255, 0, 255),
+           "Csb":QColor(150, 255, 0, 255),
+           "Csc":QColor(200, 255, 0, 255),
+           "Cwa":QColor(180, 100, 0, 255),
+           "Cwb":QColor(150, 100, 0, 255),
+           "Cwc":QColor(90, 60, 0, 255),
+           "Cfa":QColor(0, 50, 0, 255),
+           "Cfb":QColor(0, 80, 0, 255),
+           "Cfc":QColor(0, 120, 0, 255),
+           "Dsa":QColor(255, 110, 255, 255),
+           "Dsb":QColor(255, 180, 255, 255),
+           "Dsc":QColor(230, 200, 255, 255),
+           "Dsd":QColor(200, 200, 200, 255),
+           "Dwa":QColor(200, 180, 255, 255),
+           "Dwb":QColor(154, 127, 179, 255),
+           "Dwc":QColor(135, 89, 179, 255),
+           "Dwd":QColor(111, 36, 179, 255),
+           "Dfa":QColor(50, 0, 50, 255),
+           "Dfb":QColor(100, 0, 100, 255),
+           "Dfc":QColor(200, 0, 200, 255),
+           "Dfd":QColor(200, 20, 133, 255),
+           "ET": QColor(100, 255, 255, 255),
+           "EF": QColor(100, 150, 255, 255)
+           }
 
-labels = ["Af","Am","Aw","BWh","BWk","BSh","BSk","Csa","Csb","Csc","Cwa","Cwb","Cwc","Cfa","Cfb","Cfc","Dsa","Dsb","Dsc","Dsd","Dwa","Dwb","Dwc","Dwd","Dfa","Dfb","Dfc","Dfd","ET","EF"]
+l1 = [QgsColorRampShader.ColorRampItem(i+1,diction[val],val) for i, val in enumerate(diction)]
 
-lst = []
-print(sorted_values, len(sorted_values))
-for i,val in enumerate(sorted_values):
-    lst.append(QgsColorRampShader.ColorRampItem(val,colList[i],labels[i]))
-
-qCRS.setColorRampItemList(lst)
+qCRS.setColorRampItemList(l1)
 qCRS.setColorRampType(QgsColorRampShader.Exact)
 
 shader = QgsRasterShader()
